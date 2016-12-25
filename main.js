4bbbe750d5e4eca982833f651216724f20bfb34d
AnkitPatanaik <apatanai@purdue.edu>
2016-12-25 13:17:53 -0800

    can listen to commands if usingsadbot echo cmd

--- main.js
+++ main.js
@@ -11,21 +11,45 @@ var client = tumblr.createClient({
         token_secret: config.get('tokenSecret')
 });
 
-var options = {
-        caption: 'Test',
-        source: 'https://i.imgur.com/OBXukWT.jpg',
-};
-client.createPhotoPost('ankitpancakes', options, function() {
-    console.log('successfully posted picture');
-});
-
 //login with facebook 
 login({
         email: config.get('fbUsername'), 
         password: config.get('fbPassword')
     }, loginCallback);
 
-function loginCallback(){
-        console.log("helloworld");
+//helpers below
+function loginCallback(err, api) {
+         if(err) return console.error(err);
+         console.log('hello world');
+         api.listen(function callback(err, message) {
+                 console.log('HELLO I AM HERE');
+                 if(isCommand(message)) {
+                         console.log('wya');
+                         var commandString = message.body.slice(8);
+                         console.log(commandString);
+                 }
+         });
 }
+
+//checks if message is a command aka starts with slash
+function isCommand(message) {
+        if (!(message && message.body)) {
+             return false;
+         } else {
+             //return true if starts with slash    
+             return message.body.startsWith('/tumblr');
+         }
+}
+
+//post pictures to tumblr
+var options = {
+        caption: 'Test',
+        source: 'https://i.imgur.com/OBXukWT.jpg',
+};
+
+/*
+client.createPhotoPost('ankitpancakes', options, function() {
+    console.log('successfully posted picture');
+});
+*/
 
