7f8c29d6349538c3a63c80d54de582844950b7f7
AnkitPatanaik <apatanai@purdue.edu>
2016-12-25 17:38:49 -0800

    refactor a bit

--- main.js
+++ main.js
@@ -5,51 +5,50 @@ var OAuth = require('oauth');
 
 //tumblr credentials
 var client = tumblr.createClient({
-        consumer_key: config.get('consumerKey'),
-        consumer_secret: config.get('consumerSecret'),
-        token: config.get('token'),
-        token_secret: config.get('tokenSecret')
+    consumer_key: config.get('consumerKey'),
+    consumer_secret: config.get('consumerSecret'),
+    token: config.get('token'),
+    token_secret: config.get('tokenSecret')
 });
 
 //login with facebook 
 login({
-        email: config.get('fbUsername'), 
-        password: config.get('fbPassword')
+    email: config.get('fbUsername'), 
+    password: config.get('fbPassword')
     }, loginCallback);
 
-//helpers below
+//
 function loginCallback(err, api) {
-        if(err) return console.error(err);
-        api.setOptions({selfListen: true});
-        api.listen(function callback(err, message) {
-                if(isCommand(message)) {
-                        var commandString = message.body.slice(8);
-                        console.log(commandString);
-                        var cmdArray = commandString.split(',');
-                        var imgURL = cmdArray[0];
-                        var caption = cmdArray[1];
-                        console.log(imgURL);
-                        console.log(caption);
-                        
-                        //post pictures to tumblr
-                        var options = {
-                            caption: caption,
-                            source: imgURL,
-                        };
+    if(err) return console.error(err);
+    //so that you read your own messages
+    api.setOptions({selfListen: true});
+    api.listen(function callback(err, message) {
+        if(isCommand(message)) {
+            var commandString = message.body.slice(8);
+            var cmdArray = commandString.split(',');
+            
+            var imgURL = cmdArray[0];
+            var caption = cmdArray[1];
+            
+            //post pictures to tumblr
+            var options = {
+                caption: caption,
+                source: imgURL,
+            };
 
-                        client.createPhotoPost('ankitpancakes', options, function() {
-                            console.log('successfully posted picture');
-                        });
-                 }
-         });
+            client.createPhotoPost(config.get('tumblrUsername', options, function() {
+                console.log('successfully posted picture');
+            });
+        }
+    });
 }
 
 //checks if message is a command aka starts with slash
 function isCommand(message) {
-        if (!(message && message.body)) {
-             return false;
-         } else {
-             //return true if starts with slash    
-             return message.body.startsWith('/tumblr');
-         }
+    if (!(message && message.body)) {
+        return false;
+    } else {
+        //return true if starts with slash    
+        return message.body.startsWith('/tumblr');
+    }
 }
